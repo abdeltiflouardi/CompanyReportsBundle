df9738bdf0dc29acd861cbef87e4fb912f6bde7b
ouardisoft <louardi.abdeltif@yahoo.fr>
2012-08-14 12:00:39 +0200

    Update Validator/Constraint/TVANumber.php
    Fix french tva format

--- Validator/Constraint/TVANumber.php
+++ Validator/Constraint/TVANumber.php
@@ -12,7 +12,7 @@ class TVANumber extends Constraint
 
     public $message  = 'tva.number.error';
     public $patterns = array(
-        'fr' => '^FR[0-9]{11}$',
+        'fr' => '^FR[A-Za-z0-9]{2}[0-9]{9}$',
         'es' => '^ES[\w\d]{1}[0-9]{7}[\w\d]{1}$',
         'it' => '^IT[0-9]{11}$',
     );
